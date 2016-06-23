d14c2244de270e111700c0d24071ae605328c3df
Niels Krijger <niels@kryger.nl>
2016-06-23 20:13:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Logger
+
+**NOTE**: this library is not very customizable nor will it be, it's intent is to provide a standard for my projects.
+
+# Install
+
+```
+npm install --save @nielskrijger/logger
+```
+
+# Configuration
+
+Use the library like this:
+
+```
+import log from '@nielskrijger/logger';
+
+log.init({
+  console: {
+    level: 'debug',
+  },
+  loggly: {
+    level: 'info',
+    subdomain: 'test',
+    token: '99f026e6-fb0b-443c-8b1b-c6e666f5a6c1',
+    tags: ['my-domain'],
+  },
+});
+
+log.debug('Debug this', { object: 'value' });
+log.info('Starting server');
+log.warn('Watch out!');
+log.error('Error', { test: 'Extra properties!' });
+```
+
+**Options**
+
+- `console.level`: possible values `none`, `debug`, `info`, `warn`, `error`. To disable all logging set to `none`. Default `none`.
+- `loggly.level`: possible values `none`, `debug`, `info`, `warn`, `error`. To disable all logging set to `none`. Default `none`.
+- `loggly.token`: the Loggly token, required when `loggly.level !== 'none'`.
+- `loggly.subdomain`: the Loggly subdomain, required when `loggly.level !== 'none'`.
+- `loggly.tags`: tags added to the log statement. The `os.hostname()` is always added in addition to any tags you specify. Default `[]`.
---
+++ src/index.js
@@ -0,0 +1,72 @@
+import {
+  Logger as WinstonLogger,
+  transports as Transports,
+} from 'winston';
+import 'winston-loggly';
+import os from 'os';
+
+/**
+ * Configures logger.
+ */
+class Logger {
+
+  constructor() {
+    this.logger = null;
+  }
+
+  init(config) {
+    const transports = [];
+    const cfg = Object.assign({
+      console: {
+        level: 'none',
+      },
+      loggly: {
+        level: 'none',
+        token: null,
+        subdomain: null,
+        tags: [],
+      },
+    }, config);
+
+    if (cfg.console.level !== 'none') {
+      transports.push(new Transports.Console({
+        colorize: true,
+        prettyPrint: true,
+        timestamp: true,
+        level: cfg.console.level,
+      }));
+    }
+
+    console.log([os.hostname()].concat(config.tags))
+
+    if (cfg.loggly.level !== 'none') {
+      transports.push(new Transports.Loggly({
+        level: cfg.loggly.level,
+        inputToken: cfg.loggly.token,
+        subdomain: cfg.loggly.subdomain,
+        tags: [os.hostname()].concat(config.tags).filter(tag => tag !== undefined),
+        json: true,
+      }));
+    }
+
+    this.logger = new WinstonLogger({ transports });
+  }
+
+  debug() {
+    this.logger.debug.apply(this, arguments);
+  }
+
+  info() {
+    this.logger.info.apply(this, arguments);
+  }
+
+  warn() {
+    this.logger.warn.apply(this, arguments);
+  }
+
+  error() {
+    this.logger.error.apply(this, arguments);
+  }
+}
+
+export default new Logger();
